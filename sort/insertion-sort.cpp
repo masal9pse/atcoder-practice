a08dc3e0a5e1e024f5a5c84b1a6d65d6d88ee88d
masal9pse <returnymgstokh@icloud.com>
2022-03-09 01:48:46 +0900

    C++で挿入ソートを実装

---
+++ sort/insertion-sort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+// 5 2 4 6 1 3
+//
+int main()
+{
+    int N, count = 0;
+    cin >> N;
+    int A[N];
+    // std::sort(input.begin(), input.end());
+    // std::cout << input << std::endl;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+        int c = 1;
+    }
+
+    for (int i = 1; i < N; i++)
+    {
+        int temp = A[i];
+        int j = i - 1;
+        int b = 1;
+        while (j >= 0 && A[j] > temp)
+        {
+            A[j + 1] = A[j];
+            j --;
+            int c = 1;
+        }
+        A[j + 1] = temp;
+        int o = 3;
+        // if (j >= 0)
+        // {
+        //     A[j + 1] = A[j];
+        //     int c = 1;
+        // }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    return EXIT_SUCCESS;
+}
+
+// vector<int> insertionSort () {
+
+// }

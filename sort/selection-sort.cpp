b632ed89dd28db690d17628c0bfcf2b954fa2ccf
masal9pse <returnymgstokh@icloud.com>
2022-03-09 15:58:47 +0900

    dartで商の求め方

--- sort/selection-sort.cpp
+++ sort/selection-sort.cpp
@@ -46,38 +46,3 @@ int main()
   cout << endl << count << endl;
   return 0;
 }
-
-// 2 5 3
-// void selectionSort()
-// {
-//   for (int i = 0; i < n; i++)
-//   {
-//     int min_idx_temp = i;
-//     for (int j = i + 1; j < n; i++)
-//     {
-//       if (a[min_idx_temp] > a[j])
-//       {
-//         min_idx_temp = j;
-//       }
-//       if (i != min_idx_temp)
-//       {
-//         int temp = a[i];
-//         a[i] = a[min_idx_temp];
-//         a[min_idx_temp] = temp;
-//         int o = 3;
-//       }
-//     }
-//   }
-//   out();
-// }
-
-// void out()
-// {
-//   for (int i = 0; i < n; i++)
-//   {
-//     if (i)
-//       cout << ' ';
-//     cout << a[i];
-//   }
-//   cout << endl;
-// }
---
+++ sort/shell-sort.dart
@@ -0,0 +1,11 @@
+void main() {
+  // print(shellSort([2,3,]));
+  print(shellSort([5,6,9,2,3]));
+}
+
+// https://note.com/hatchoutschool/n/n0eb12dd9ffa1
+List<int> shellSort(List<int> numbers) {
+  final lenNumbers = numbers.length;
+  final gap = lenNumbers ~/ 2;
+  return [1];
+}

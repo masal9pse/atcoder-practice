25c73c1f94c8aae5312b819c8b2e998f0fb30420
masal9pse <returnymgstokh@icloud.com>
2022-03-16 21:54:15 +0900

    問題文の３ケースは全てパスした

--- abc/a.cpp
+++ abc/a.cpp
@@ -9,35 +9,12 @@ int main()
     cin >> V >> A >> B >> C;
     int count = 0;
     string result;
-    // if (V < A || V - (A + B + C) == 0) {
-    // // if (V < A || V <= (A + B + C)) {
-    //     result = "F";
-    // // } else if (V - A == 0 || V < A + B) {
-    // } else if (V <= A || V < A + B) {
-    //     result = "M";
-    // // } else if (V - (A+B) <= 0 || V < A + B + C){
-    // } else if (V <= (A+B)|| V < A + B + C){
-    //   result = "T";
-    // }
-    // for (int i = 0; i < 10000; i++)
-    // {
-
-    // }
     while (V >= 0)
     {
         int calc1 = A + B;
-        int calc2 = A + B + C;
-        //   if (V > A) {
-        //      V = V - A;
-        //   } else if (V > A + B) {
-        //      V = V - (A+B);
-        //   } else if (V > A + B + C) {
-        //     V = V - (A + B + C);
-        //   }
-        // int calc1 = A + B;
-        // int calc2 = A + B + C;
+        int calc2 = A + B + C;        
         result = 'F';
-        if (V > A)
+        if (V >= A)
         {
             V -= A;
             result = 'M';
@@ -45,7 +22,7 @@ int main()
             break;
         }
         // if (V > A + B || V == A)
-        if (V > B || V == A)
+        if (V >= B || V == A)
         {
             // V = V - (A + B);
             V -=B;
@@ -55,7 +32,7 @@ int main()
         }
 
         // if (V > A + B + C || V == A + B)
-        if (V > C || V == B)
+        if (V >= C || V == B)
         {
             // V = V - (A + B + C);
             V -= C;

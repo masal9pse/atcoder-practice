f375def46de349aabac9d9722eb9ff28fe4695bf
masal9pse <returnymgstokh@icloud.com>
2022-03-18 04:30:09 +0900

    メモ化再帰？で早く実行が完了した。

--- abc/240/c2.cpp
+++ abc/240/c2.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
-// #define rep(i,n) for(int i = 0; i < n;i++);
-// using P = pair<int,int>;
+using P = pair<int,int>;
 int N, X;
-vector<int> a;
-vector<int> b;
+vector<int> a,b;
+map<P,bool> memo;
+
 bool f(int i, int x)
 {
     if (i == N)
         return x == X;
+    P arg(i,x);
+    if (memo.count(arg)) {
+        return memo[arg];
+    }
     bool res = false;
     if (f(i + 1, x + a[i]) || f(i + 1, x + b[i]))
         res = true;
-    return res;
+    return memo[P(i,x)] = res;
 }
 
 int main()

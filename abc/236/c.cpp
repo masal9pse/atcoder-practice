7262e52e9ffbb90c2c108eb074a495e0a972e823
masal9pse <returnymgstokh@icloud.com>
2022-03-20 13:29:11 +0900

    エラー

--- abc/236/c.cpp
+++ abc/236/c.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <vector>
-// #include <string>
 using namespace std;
 
-//  int N;    
+//  int N;
 //     cin >> N;
 //     vector<string> S(N);
 //     for (int i = 0; i < N; i++) cin >> S[i]
 int main()
 {
-    int n,m;
+    int n, m;
     cin >> n >> m;
     vector<string> s[n];
     vector<string> t[m];
-    for (int i = 0; i < n; i++) {
-       cin >> s[i];
-    }
-    for (int i = 0; i < m; i++) cin >> t[i];
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    for (int i = 0; i < m; i++)
+        cin >> t[i];
 
     for (int i = 0; i < n; i++)
     {
         bool flag = false;
         for (int j = 0; j < m; j++)
         {
-          if (s[i] == t[j]) {
-              flag = true;
-          }
+            if (s[i] == t[j])
+            {
+                flag = true;
+            }
         }
-        if (flag) cout << "Yes" << endl;
-        else cout << "No" << endl;
+        if (flag)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
     }
     return 0;
 }

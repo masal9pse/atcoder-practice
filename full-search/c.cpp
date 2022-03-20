286198b047374ebd953b9926d45f64cc8bf481d4
masal9pse <returnymgstokh@icloud.com>
2022-03-20 13:47:25 +0900

    エラー

---
+++ full-search/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//  int N;
+//     cin >> N;
+//     vector<string> S(N);
+//     for (int i = 0; i < N; i++) cin >> S[i]
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> s[n];
+    for (int i = 0; i < n; i++) cin >> s[i];
+    // vector<string> t[m];
+    // for (int i = 0; i < m; i++) cin >> t[i];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     bool flag = false;
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         if (s[i] == t[j])
+    //         {
+    //             flag = true;
+    //         }
+    //     }
+    //     if (flag)
+    //         cout << "Yes" << endl;
+    //     else
+    //         cout << "No" << endl;
+    // }
+    return 0;
+}

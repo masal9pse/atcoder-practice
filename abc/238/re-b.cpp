f60cfdb5a383cf5fbfdd20a2ec0b03961c3ae0f6
masal9pse <returnymgstokh@icloud.com>
2022-07-11 19:22:05 +0900

    隣同士を足し合わせる関数を定義

---
+++ abc/238/re-b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <regex>
+#include <iomanip>
+#include <map>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < n; i++)
+
+// ランレングス圧縮
+vector<P> rle(const string &s)
+{
+    vector<P> res;
+    for (char c : s)
+    {
+        if (res.size() > 0 && res.back().first == c)
+        {
+            res.back().second++;
+        }
+        else
+        {
+            res.emplace_back(c, 1);
+        }
+    }
+    return res;
+}
+
+vector<int> getNextSumList(vector<int> a,int n)
+{
+    int sum = 0;
+    vector<int> b(n);
+    rep(i, n)
+    {
+        sum += a[i];
+        b[i] += sum;
+    }
+    return b;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    rep(i, n) cin >> a[i];
+    int sum = 0;
+    b = getNextSumList(a,n);
+    return 0;
+}

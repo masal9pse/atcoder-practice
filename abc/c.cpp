ad885fa5b6269b9ea838bd3d61db7ccbc8dcfa60
masal9pse <returnymgstokh@icloud.com>
2023-11-25 19:36:46 +0900

    today commit

--- abc/c.cpp
+++ abc/c.cpp
@@ -20,30 +20,55 @@ using mi = map<int, int>;
 #define rep2(i, n) for (int i = 1; i <= n; i++)
 #define rep3(i, n) for (int i = 0; i <= n; i++)
 const double PI = acos(-1);
-// テストケース
-// https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAA3C0ZYYEnFXF6QopVCKxjNa/abc329/C?dl=0&subfolder_nav_tracking=1
+
 int main()
 {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-    vector<pair<char,int> > d;
-    d.emplace_back(s[0],0);
-    // 部分文字列に分割する
-    for (auto c:s)
+    vector<vector<int>> d(9, vector<int>(9));
+    rep(i, 9) rep(j, 9) cin >> d.at(i).at(j);
+    // vector<vector<bool>> f(9,vector<bool>(9));
+    rep(i, 9)
     {
-        if (d.back().first == c) d.back().second++;
-        else d.emplace_back(c,1);
+        vector<bool> first_check_list(9);
+        vector<bool> second_check_list(9);
+        rep(j, 9)
+        {
+            first_check_list[d[i][j] - 1] = true;
+            second_check_list[d[j][i] - 1] = true;
+        }
+        rep(j, 9)
+        {
+            if (!first_check_list[j] || !second_check_list[j])
+            {
+                cout << "No" << endl;
+                return 0;
+            }
+        }
+
+        // ケース3
+        rep(j, 9)
+        {
+            if (i % 3 == 0 && j % 3 == 0)
+            {
+                vector<bool> third_check_list(9);
+                rep(a, 3)
+                {
+                    rep(b, 3)
+                    {
+
+                        int row_value = d[i + a][j + b] - 1;
+                        int column_value = d[b + j][i + a] - 1;
+                        third_check_list.at(row_value) = true;
+                        third_check_list.at(column_value) = true;
+                    }
+                }
+                if (!third_check_list[j])
+                {
+                    cout << "No" << endl;
+                    return 0;
+                }
+            }
+        }
     }
-    vector<int> ans(26);
-    rep(i,d.size()) {
-        ans[d[i].first -'a'] = max(ans[d[i].first-'a'],d[i].second);
-    }
-    int sum = 0;
-    rep(i,26) {
-        sum += ans[i];
-    }
-    cout << sum << endl;
+    cout << "Yes" << endl;
     return 0;
 }

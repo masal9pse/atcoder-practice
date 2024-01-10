c6707b184fad810eab5edd57dd6200c23307c758
masal9pse <returnymgstokh@icloud.com>
2024-01-10 19:31:59 +0900

    today commit

--- abc/301~350/333/d4.cpp
+++ abc/301~350/333/d4.cpp
@@ -13,7 +13,8 @@
 using namespace std;
 using ll = long long;
 // using P = pair<int, int>;
-template<class T> using P = pair<T, T>;
+template <class T>
+using P = pair<T, T>;
 using G = vector<vector<int>>;
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define rep2(i, n) for (int i = 1; i <= n; i++)
@@ -34,9 +35,12 @@ int main()
   /*
     全て目安20m　ただ手が動くうちはエンドレスでやる。これによってコンテスト本番の粘りACや思考力のupにつながる
     問題文の理解 read
-      最大値、最小値の「総和」ってこと？
-      問題文の読解ができていないので飛ばし
     実装方針決め plan
+      1に紐づく部分和グラフの最大値を求める
+      もうここに書き込む
+      もとの木から頂点 1 と頂点 1 に接続する辺を取り除くと、森ができます。 そして（頂点 1 でない）ある頂点を根としたときの、頂点 1 を根とする部分木の大きさは、n から (根として選んだ頂点が属する連結成分の大きさ) を引いたものになります。
+
+　　n から森の連結成分の大きさのうち最大のものを引いたものになります。
     実装 do
     ３つのパートに分け、それぞれに時間制限を設けることで以下のメリットがある
      - すぐに解説を見てしまう癖を防止できる
@@ -48,5 +52,35 @@ int main()
   */
   int n;
   cin >> n;
+  G to(n);
+  rep(i, n - 1)
+  {
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    to[u].push_back(v);
+    to[v].push_back(u);
+  }
+  if (to[0].size() == 1) {
+    cout << 1 << endl;
+    return 0;
+  }
+  int ans = n;
+  for (int u : to[0])
+  {
+    auto dfs = [&](auto f, int v, int p = -1) -> int
+    {
+      int res = 1;
+      for (int u : to[v])
+      {
+        if (p == u) continue;
+        res += f(f, u, v);
+      }
+      return res;
+    };
+    int now = n - dfs(dfs, u, 0);
+    ans = min(ans,now);
+  }
+  cout << ans << endl;
   return 0;
 }
---
+++ memo.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <algorithm>
+#include <set>
+#include <regex>
+#include <iomanip>
+#include <map>
+#include <cassert>
+#include <stack>
+#include <queue>
+using namespace std;
+using ll = long long;
+// using P = pair<int, int>;
+template <class T>
+using P = pair<T, T>;
+using G = vector<vector<int>>;
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep2(i, n) for (int i = 1; i <= n; i++)
+#define rep3(i, n) for (int i = 0; i <= n; i++)
+const double PI = acos(-1);
+const int MI = 10e8;
+const ll MLL = 1e18;
+// 4方向
+// int di[4] = {1, 0, -1, 0};
+// int dj[4] = {0, 1, 0, -1};
+
+// 8方向
+const vector<int> di = {-1, -1, 0, 0, 1, 1, 1, -1};
+const vector<int> dj = {0, 1, -1, 1, -1, 0, 1, -1};
+
+int main()
+{
+    /*
+      全て目安20m　ただ手が動くうちはエンドレスでやる。これによってコンテスト本番の粘りACや思考力のupにつながる
+      問題文の理解 read
+      実装方針決め plan
+      実装 do
+      ３つのパートに分け、それぞれに時間制限を設けることで以下のメリットがある
+       - すぐに解説を見てしまう癖を防止できる
+       - １問に何時間も粘りすぎてしまう問題を防止できる
+
+      解説記事見た article
+        理解すること＋どうやったらその問題を初見で解けるか考える
+      解説動画見た video
+    */
+    int n, d;
+    cin >> n >> d;
+    vector<string> s(n);
+    rep(i, n) cin >> s[i];
+    int ans = 0;
+    int count = 0;
+    rep(i, d)
+    {
+        bool flag = true;
+        rep(j, n)
+        {
+            if(s[j][i] == 'x') flag = false;
+        }
+        if (flag) count++;
+        else count = 0;
+        ans = max(count,ans);
+    }
+    cout << ans << endl;
+    return 0;
+}
